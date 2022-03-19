21c5ffcf4062bb3cf9f3ad13be938139f77d27a9
balbino aylagas <balbino_a@hotmail.com>
2022-03-19 12:12:22 -0600

    started order create flow

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from routes.auth import auth
 from routes.todolist import todolist
+from routes.orders import orders
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from utils.loginManagerService import login_manager
@@ -18,3 +19,4 @@ Migrate(app, db)
 
 app.register_blueprint(auth)
 app.register_blueprint(todolist)
+app.register_blueprint(orders)
